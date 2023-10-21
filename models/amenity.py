180c418748e026709cfa16bbb36795b6a85b61bf
kawakibb <kawakibk7@gmail.com>
2023-10-21 21:33:49 +0000

    python ORM

--- models/amenity.py
+++ models/amenity.py
@@ -1,4 +1,17 @@
-from models.base_model import BaseModel
+#!/usr/bin/python3
+"""
+  the amenity class
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
-class Amenity(BaseModel):
-    name = ""
+
+class Amenity(BaseModel, Base):
+    """
+    This is the class for Amenity
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
