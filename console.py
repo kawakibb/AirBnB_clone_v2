4e13d4f6d4ce7ad408c6116ecd597a2175b1d250
kawakibb <kawakibk7@gmail.com>
2023-10-21 20:10:30 +0000

    python ORM

--- console.py
+++ console.py
@@ -65,6 +65,32 @@ class HBNBCommand(cmd.Cmd):
         """Handle EOF (Ctrl+D) to exit the program"""
         print("")
         return True
+ 
+   def do_create(self, args):
+        """ Create an object of any class"""
+        try:
+            if not args:
+                raise SyntaxError()
+            arg_list = args.split(" ")
+            kw = {}
+            for arg in arg_list[1:]:
+                arg_splited = arg.split("=")
+                arg_splited[1] = eval(arg_splited[1])
+                if type(arg_splited[1]) is str:
+                    arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
+                kw[arg_splited[0]] = arg_splited[1]
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
+        new_instance.save()
+        print(new_instance.id)
+
+    def help_create(self):
+        """ Help information for the create method """
+        print("Creates a class of any type")
+        print("[Usage]: create <className>\n")
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
