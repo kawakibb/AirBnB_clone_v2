fa3da25bc9f798fd64595c8669c0fdf4d7e52319
kawakibb <kawakibk7@gmail.com>
2023-10-21 21:42:05 +0000

    python ORM

--- models/review.py
+++ models/review.py
@@ -1,6 +1,18 @@
-from models.base_model import BaseModel
+#!/usr/bin/python3
+"""
+the review class
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
-class Review(BaseModel):
-    place_id = ""
-    user_id = ""
-    text = ""
+
+class Review(BaseModel, Base):
+    """
+    the class for Review
+    place id user id review description
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
