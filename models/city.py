d21033a43f443f5717a1a577150f08fbc328ec86
kawakibb <kawakibk7@gmail.com>
2023-10-21 21:36:02 +0000

    python ORM

--- models/city.py
+++ models/city.py
@@ -1,5 +1,22 @@
-from models.base_model import BaseModel
+#!/usr/bin/python3
+"""
+the city class
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
-class City(BaseModel):
-    state_id = ""
-    name = ""
+
+class City(BaseModel, Base):
+    """
+    This is the class for City
+    The state id
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
