889f20ecbbf0fb58be6a4eb77d94d587de229257
Zineb112 <zbelassiri@gmail.con>
2023-10-22 14:07:17 -0700

    models/engine/file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,38 +1,63 @@
-#!/usr/bin/python3
 import json
-from os.path import exists
+import os
+
 
 class FileStorage:
-    # Path to the JSON file
     __file_path = "file.json"
-    # Dictionary to store objects by class name and id
     __objects = {}
 
-    def all(self):
-        # Return the dictionary of objects
-        return self.__objects
+    def all(self, cls=None):
+        """
+        Return a dictionary of all objects,
+        or objects of a specific class if cls is provided.
+        """
+        if cls is None:
+            return self.__objects
+        else:
+            filtered_objects = {}
+            for key, obj in self.__objects.items():
+                if obj.__class__ == cls:
+                    filtered_objects[key] = obj
+            return filtered_objects
 
     def new(self, obj):
-        # Set a new object in the dictionary with key: <class name>.id
+        """
+        Add an object to the storage.
+        """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        # Serialize __objects to JSON and save it to the file
-        obj_dict = {}
+        """
+        Serialize __objects to JSON and save it to the file.
+        """
+        serialized_objects = {}
         for key, obj in self.__objects.items():
-            obj_dict[key] = obj.to_dict()
-        with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump(obj_dict, file)
+            serialized_objects[key] = obj.to_dict()
+
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
-        # Deserialize JSON from the file to __objects (if the file exists)
-        if exists(self.__file_path):
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                obj_dict = json.load(file)
-            for key, obj_data in obj_dict.items():
-                class_name, obj_id = key.split(".")
-                cls = class_name
-                obj_dict[key]["created_at"] = datetime.strptime(obj_dict[key]["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
-                obj_dict[key]["updated_at"] = datetime.strptime(obj_dict[key]["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
-                self.__objects[key] = eval(cls)(**obj_data)
+        """
+        Deserialize the JSON file to __objects.
+        """
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, obj_dict in data.items():
+                    class_name = obj_dict['__class__']
+                    cls = eval(class_name)
+                    obj = cls(**obj_dict)
+                    self.__objects[key] = obj
+
+    def delete(self, obj=None):
+        """
+        Delete an object from __objects if it exists.
+        If obj is None, the method does nothing.
+        """
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
