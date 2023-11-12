8cbf7549098770f5731e80d0d2a9391251548c1c
kawakibb <kawakibk7@gmail.com>
2023-11-12 13:56:32 +0000

    rbnb_clone

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["100.26.231.2", "100.26.213.235"]
 
 
 def do_deploy(archive_path):
